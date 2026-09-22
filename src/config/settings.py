88037f6728281b702d9eba6c5a77f9557b549ab8
Luis Garizao <luisangelgarizao2004@gmail.com>
2026-09-21 23:00:55 -0500

    Prod allowed hosts hotfix - 23:00:55 - 2026-09-21

--- src/config/settings.py
+++ src/config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
